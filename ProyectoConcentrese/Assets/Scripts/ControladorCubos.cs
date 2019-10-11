f7c1c862895d770f1464d9afe34a2b69ce9865a1
Carlos Fernando Villa Berrio <carlos.shizuken@gmail.com>
2019-10-11 11:16:16 -0500

    avance

---
+++ ProyectoConcentrese/Assets/Scripts/ControladorCubos.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorCubos : MonoBehaviour
+{
+
+    void Update()
+    {
+        DeterminaParecido();
+    }
+
+    void DeterminaParecido()
+    {
+        GameObject cubo_1 = null;
+        GameObject cubo_2 = null;
+        foreach (var item in FindObjectsOfType<LogicaCubo>())
+        {
+            if(item.seleccionado == true && cubo_1 == null)
+            {
+                cubo_1 = item.esteCubo;
+            }
+            if(item.seleccionado == true && cubo_1 != item.esteCubo && cubo_2 == null)
+            {
+                cubo_2 = item.esteCubo;
+            }
+
+            if (cubo_1 != null && cubo_2 != null && cubo_1.transform.rotation.eulerAngles.y > -180f && cubo_2.transform.rotation.eulerAngles.y > -180f)
+            {
+                if (cubo_1 != null && cubo_2 != null && cubo_1.GetComponent<LogicaCubo>().material == cubo_2.GetComponent<LogicaCubo>().material)
+                {
+                    cubo_1.GetComponent<LogicaCubo>().correcto = true;
+                    cubo_2.GetComponent<LogicaCubo>().correcto = true;
+                    cubo_1.GetComponent<LogicaCubo>().seleccionado = false;
+                    cubo_2.GetComponent<LogicaCubo>().seleccionado = false;
+                }
+            }
+        }
+        if (cubo_1 != null && cubo_2 != null && cubo_1.GetComponent<LogicaCubo>().correcto == false && cubo_2.GetComponent<LogicaCubo>().correcto == false)
+        {
+            cubo_1.GetComponent<LogicaCubo>().sePuedeOcultar = true;
+            cubo_2.GetComponent<LogicaCubo>().sePuedeOcultar = true;
+            cubo_1 = null;
+            cubo_2 = null;
+        }
+    }
+}
---
+++ ProyectoConcentrese/Assets/Scripts/LogicaCubo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaCubo : MonoBehaviour
+{
+    public bool sePuedeVoltear;
+    public bool sePuedeOcultar;
+    public Material material;
+    public bool seleccionado;
+    public bool correcto;
+    public GameObject esteCubo;
+    public int contador = 0;
+
+    void Awake()
+    {
+        //material = gameObject.GetComponentInChildren<Renderer>().material;
+        esteCubo = this.gameObject;
+    }
+
+    void Update()
+    {
+        MostrarCubo();
+        OcultarCubo();
+    }
+
+    private void OnMouseDown() // Selecciona y comprueba si se puede girar
+    {
+        if (gameObject.transform.rotation.eulerAngles.y >= 0 && gameObject.transform.rotation.eulerAngles.y < 180)
+        {
+            sePuedeVoltear = true;
+        }
+    }
+
+    void MostrarCubo()
+    {
+        if (sePuedeVoltear) // Se muestra
+        {
+            gameObject.GetComponent<Transform>().Rotate(0f, 180f * Time.deltaTime * 0.9f, 0f);
+            seleccionado = true;
+        }
+        if (gameObject.transform.rotation.eulerAngles.y >= 180f)
+        {
+            sePuedeVoltear = false;
+        }
+    }
+
+    void OcultarCubo()
+    {
+        if (sePuedeOcultar && !correcto) // Se muestra
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0f,0f,0f);
+            seleccionado = false;
+            sePuedeOcultar = false;
+        }
+    }
+}
